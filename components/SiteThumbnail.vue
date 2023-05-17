867e8e6015def0f565329a065fa83a1886cf305e
dbelley-git <dbelley@gmail.com>
2023-05-17 16:03:35 -0400

    Still works !

--- components/SiteThumbnail.vue
+++ components/SiteThumbnail.vue
@@ -1,12 +1,8 @@
 <template>
     <div class="col">
         <div class="card shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="200" xmlns="http://www.w3.org/2000/svg"
-                role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
-                <title>Placeholder</title>
-                <rect width="100%" height="100%" fill="#55595c"></rect><text x="40%" y="50%" fill="#eceeef"
-                    dy=".3em">Thumbnail</text>
-            </svg>
+            <img class="bd-placeholder-img card-img-top" src="~/assets/images/pexels-sabrina-gelbart-249798.jpg"
+                alt="image" />
 
             <div class="card-body">
                 <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
--- layouts/thumbnails.vue
+++ layouts/thumbnails.vue
@@ -2,7 +2,11 @@
     <SiteHeader />
     <main>
         <div class="container">
+            <br />
             <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                <SiteThumbnail />
+                <SiteThumbnail />
+                <SiteThumbnail />
                 <SiteThumbnail />
                 <SiteThumbnail />
                 <SiteThumbnail />
